645d017af50ec93d3457582738e71bbf814fdf73
Jenny Matamoros <jpaomat@gmail.com>
2021-12-25 00:23:38 -0500

    Configure pipeline

---
+++ go.mod
@@ -0,0 +1,9 @@
+module mag-find-mutants-function
+
+go 1.17
+
+require (
+	github.com/aws/aws-lambda-go v1.27.1 // indirect
+	github.com/aws/aws-sdk-go v1.42.25 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Request struct {
+	ID    float64 `json:"id"`
+	Value string  `json:"value"`
+}
+
+type Response struct {
+	Message string `json:"message"`
+	Ok      bool   `json:"ok`
+}
+
+func Handler(request Request) (Response, error) {
+	fmt.Println(request)
+	return Response{
+		Message: "Hola mundo",
+		Ok:      true,
+	}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
