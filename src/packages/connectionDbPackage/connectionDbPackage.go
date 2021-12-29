136b9fa51445942c2367b1c1ee6ccbbfbef57c3a
Jenny Matamoros <jpaomat@gmail.com>
2021-12-28 21:36:23 -0500

    Fixinng error for mysql module not imported

--- src/packages/connectionDbPackage/connectionDbPackage.go
+++ src/packages/connectionDbPackage/connectionDbPackage.go
@@ -7,7 +7,6 @@ import (
 	sm "mag-stadistics-dna-processed-function/src/packages/secretManagerPackage"
 
 	"database/sql"
-	"time"
 
 	"github.com/aws/aws-sdk-go/service/secretsmanager"
 	_ "github.com/go-sql-driver/mysql"
@@ -34,25 +33,16 @@ func (secretNameDb SecretNameDb) GetConnectionDb() *sql.DB {
 	getSecretDb := sm.New(secretNameDb.secretName)
 	secretDataDb := getSecretDb.GetSecretVal()
 	credentialsDb := SecretManager(secretDataDb)
-	// sqlConnection := fmt.Sprintf("%s:%s@%s/%s", credentialsDb.UserName, credentialsDb.Password, credentialsDb.Hostname, credentialsDb.Database)
-	sqlConnection := fmt.Sprintf("%s:%s@/%s", credentialsDb.UserName, credentialsDb.Password, credentialsDb.Database)
+	sqlConnection := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", credentialsDb.UserName, credentialsDb.Password, credentialsDb.Hostname, credentialsDb.Port, credentialsDb.Database)
 	fmt.Println("sqlConnection", sqlConnection)
 	db, err := sql.Open("mysql", sqlConnection)
+	// if there is an error opening the connection, handle it
 	if err != nil {
-		panic(err)
+		panic(err.Error())
 	}
-	// See "Important settings" section.
-	db.SetConnMaxLifetime(time.Minute * 3)
-	db.SetMaxOpenConns(10)
-	db.SetMaxIdleConns(10)
-	// db, err := sql.Open("mysql", sqlConnection)
-	// // if there is an error opening the connection, handle it
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-	// // defer the close till after the main function has finished
-	// // executing
-	// defer db.Close()
+	// defer the close till after the main function has finished
+	// executing
+	defer db.Close()
 	return db
 }
 
