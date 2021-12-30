21a37764832b0cd605e6a634853177073ada65b5
Cristhian Yecid Ramirez Trujillo <1049630126@ccb.org.co>
2021-12-30 15:08:28 -0500

    Fixing error}

--- src/controllers/stadisticsController/stadisticsController.go
+++ src/controllers/stadisticsController/stadisticsController.go
@@ -2,12 +2,20 @@ package stadisticscontroller
 
 import (
 	"fmt"
-	db "mag-stadistics-dna-processed-function/src/packages/connectionDbPackage"
+	"mag-stadistics-dna-processed-function/src/config/connections"
+	"mag-stadistics-dna-processed-function/src/config/constants"
+	"mag-stadistics-dna-processed-function/src/controllers/stadisticsController"
 )
 
 func GetStadisticsDnaProcessed() string {
-	clsConnectiob := db.NewValue("/rds_db/mysql")
-	connectionDb := clsConnectiob.GetConnectionDb()
+	// // clsConnectiob := db.NewValue("/rds_db/mysql")
+	// connectionString := constants.GetMysqlConnectionString()
+	connectionDb, errDto := connectionDb := connections.GetConnectDBMysql(
+		constants.GetMysqlConnectionString(),
+		constants.GetMaxOpenDbConn(),
+		constants.GetMasIdleDbConn(),
+		constants.GetMaxDbLifetime(),
+	)
 	defer connectionDb.Close()
 
 	resulSql, err := connectionDb.Query("SELECT * FROM mutants_general.DNA_VERIFICATION_MUTANTS")
